6e44268f3d2b073a55a6a05ed8d55a18e5c08d8c
Kaylee Dao <kayleed2@gmail.com>
2022-08-09 10:27:49 -0500

    Pushing scripts/styles

--- scripts.js
+++ scripts.js
@@ -2,7 +2,12 @@ $(document).ready(function() {
     dynamicQuotes();
     createCard();
     createCardTwo();
+    dynamicVideos();
 });
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
 
 const dynamicQuotes = () => {
     $.ajax({
@@ -159,4 +164,92 @@ const createCardTwo = () => {
         $(".loader").hide();
     }
 })
+}
+
+const dynamicVideos = () => {
+    $.ajax({
+        method: "GET",
+        url: "https://smileschool-api.hbtn.info/courses",
+        data: {
+            q: $("#searchfield").val(),
+            topic: $("#drops").val(),
+            sort: $("#drops2").val()
+        },
+        dataType: "json",
+        beforeSend: function() {
+            $(".loader").show();
+        },
+        success: function(data) {
+        for (i of data.topics) {
+            $('#drops').append(` <button class="dropdown-item" onclick="useSearch()" type="button">${capitalizeFirstLetter(i)}</button>`)
+        }
+        for (i of data.sorts) {
+            $('#drops2').append(` <button class="dropdown-item" onclick="useSearch()" type="button">${capitalizeFirstLetter(i.replaceAll('_', ' '))}</button>`)
+        }
+        $('#drops .dropdown-item').click(function() {
+            let text = $(this).text();
+            $('#place').html(capitalizeFirstLetter(text));
+        })
+        $('#drops2 .dropdown-item').click(function() {
+            let text = $(this).text();
+            $('#sort').html(capitalizeFirstLetter(text));
+        })
+        },
+        complete: function() {
+            $(".loader").hide();
+        }
+     })
+}
+
+const useSearch = () => {
+    $.ajax({
+        method: "GET",
+        url: "https://smileschool-api.hbtn.info/courses",
+        data: {
+            q: $('[type=search]').val(),
+            topic: $('#place').text(),
+            sort: $("#sort").text()
+        },
+        dataType: "json",
+        beforeSend: function() {
+            $(".loader").show();
+        },
+        success: function(data) {
+        $("#videos").empty();
+        let key = capitalizeFirstLetter(data.q);
+        let topic = capitalizeFirstLetter(data.topic);
+        for (el of data.courses) {
+            if (topic == el.topic && $.inArray(key, el.keywords != -1)) {
+                    $("#videos").append(`
+                    <div class="card border-0 p-1">
+                    <video src="" poster="${el.thumb_url}"></video>
+                    <div class="playbtn">
+                        <a href="" class="img-fluid w-25"><img class="img-fluid" src="./images-2/play.png" alt=""></a>
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title" style="font-family: var(--font-ssp-bold);">${el.title}</h5>
+                        <p class="card-text">${el["sub-title"]}</p>
+                        <div class="d-flex mt-3 align-items-center">
+                            <img src="${el.author_pic_url}" class="rounded-circle img-fluid w-25" alt="">
+                            <p class="ml-2 mb-0 small" style="font-family: var(--font-ssp-semibold); color: #C271FF;">${el.author}</p>
+                        </div>
+                        <div class="d-flex align-items-center mt-3">
+                            <div class="d-flex w-25">
+                                <img src="./images-2/star_on.png" class="img-fluid w-25" alt="">
+                                <img src="./images-2/star_on.png" class="img-fluid w-25" alt="">
+                                <img src="./images-2/star_on.png" class="img-fluid w-25" alt="">
+                                <img src="./images-2/star_on.png" class="img-fluid w-25" alt="">
+                                <img src="./images-2/star_off.png" class="img-fluid w-25" alt="">
+                            </div>
+                            <p class="m-0" style="font-family: var(--font-ssp-semibold); color: #C271FF;">${el.duration}</p>
+                        </div>
+                    </div>
+                </div>`);
+            }
+        }
+        },
+        complete: function() {
+            $(".loader").hide();
+        }
+    })
 }
